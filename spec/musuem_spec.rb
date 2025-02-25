030b7d765c00677fdee482674245369af1bdfad3
Stephanie Guerrero <sguerrero0991@gmail.com>
2025-02-25 10:53:13 -0600

    Creates tests for iteration 2

---
+++ spec/musuem_spec.rb
@@ -0,0 +1,74 @@
+require './lib/museum'
+require './lib/exhibit'
+require './lib/patron'
+require 'pry'
+
+RSpec.describe Musuem do 
+  describe '#initialize' do
+    it 'is a Musuem' do
+      dmns = Museum.new("Denver Museum of Nature and Science")
+      expect(dmns).to be_a(Museum)
+    end
+
+    it 'has a name' do
+      dmns = Museum.new("Denver Museum of Nature and Science")
+      expect(dmns.name).to eq("Denver Museum of Nature and Science")
+    end
+
+    it 'has exhibits' do
+      dmns = Museum.new("Denver Museum of Nature and Science")
+      expect(dmns.exhibits).to eq([])
+    end
+  end
+
+  describe '#initialize' do
+    it 'is an Exhibit' do
+      gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+      dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+      imax = Exhibit.new({name: "IMAX",cost: 15})
+      expect(gems_and_minerals).to be_a(Exhibit)
+      expect(dead_sea_scrolls).to be_a(Exhibit)
+      expect(imax).to be_a(Exhibit)
+    end
+
+    it 'adds exhibits' do
+      gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+      dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+      imax = Exhibit.new({name: "IMAX",cost: 15})
+      xpect(gems_and_minerals).to be_a(Exhibit)
+      expect(dead_sea_scrolls).to be_a(Exhibit)
+      expect(imax).to be_a(Exhibit)
+
+      dmns.add_exhibit(gems_and_minerals)
+      dmns.add_exhibit(dead_sea_scrolls)
+      dmns.add_exhibit(imax)
+
+      expect(dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
+    end
+  end 
+
+  describe '#Patron' do
+    it 'adds more patrons and interests' do
+      gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+      dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+      imax = Exhibit.new({name: "IMAX",cost: 15})
+      expect(gems_and_minerals).to be_a(Exhibit)
+      expect(dead_sea_scrolls).to be_a(Exhibit)
+      expect(imax).to be_a(Exhibit)
+
+      patron_1 = Patron.new("Bob", 20)
+      expect(patron_1).to be_a(Patron)
+      patron_1.add_interest("Dead Sea Scrolls")
+      patron_1.add_interest("Gems and Minerals")
+
+      patron_2 = Patron.new("Sally", 20)
+      expect(patron_2).to be_a(Patron)
+      patron_2.add_interest("IMAX")
+    end
+
+    it 'recommends exhibits' do
+      expect(dmns.recommend_exhibits(patron_1)).to eq([dead_sea_scrolls, gems_and_minerals])
+      expect(dmns.recommend_exhibits(patron_2)).to eq([imax])
+    end
+  end
+end
