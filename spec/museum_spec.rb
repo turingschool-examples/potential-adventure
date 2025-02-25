dd43b5484c5ca419c3f6c79e1f9c3fbeb1408614
Brady Blair <bblair321@yahoo.comm>
2025-02-25 09:02:29 -0800

    Changed @museum to @dmns

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -4,14 +4,14 @@ require './lib/museum'
 
 RSpec.describe Museum do
   before(:each) do
-    @museum = Museum.new
+    @dmns = Museum.new
 
   end
 
   describe "#initialize" do
     it 'Museum to be an instance of Museum' do
 
-      expect(@museum).to be_an_instance_of(Museum)
+      expect(@dmns).to be_an_instance_of(Museum)
     end
   end  
 end  
