137329292212399e8ed5457cb224dd0bfc382c74
Ecaterina Profir <ecaterina.profir@gmail.com>
2025-02-25 12:18:57 -0600

    Created a admit method and passed in the patron examples given in the test.

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,9 +1,10 @@
 
 class Museum 
-  attr_reader :name, :exhibits
+  attr_reader :name, :exhibits, :patrons
   def initialize(name) 
     @name = name
     @exhibits = []
+    @patrons = []
   end
 
   def add_exhibit(exhibit)
@@ -14,5 +15,9 @@ class Museum
     @exhibits.find_all do |exhibit|
       patron.interests.include?(exhibit.name)
     end
+  end
+
+  def admit(patron)
+    @patrons << patron
   end
 end
