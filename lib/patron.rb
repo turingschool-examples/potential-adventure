2e630d5c3e382411be7f06fb5050553504c27bd5
Brady Blair <bblair321@yahoo.comm>
2025-02-25 10:59:40 -0800

    final try

--- lib/patron.rb
+++ lib/patron.rb
@@ -1,5 +1,6 @@
 class Patron
   attr_reader :name, :spending_money, :interests
+  attr_writer :spending_money
   def initialize(name,spending_money)
     @name = name
     @spending_money = spending_money
