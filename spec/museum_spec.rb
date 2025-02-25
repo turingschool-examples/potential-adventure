769454688ec2fde6e06fc4547fd04913589dd816
Ecaterina Profir <ecaterina.profir@gmail.com>
2025-02-25 11:18:56 -0600

    I created a test for the recommend_exihits. I added the patrons to this class in order to access their information. Their interests name match the name of the exhibits and i  used those to identify the expected output of the ethod.

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -8,7 +8,8 @@ RSpec.describe Museum do
     @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
     @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
     @imax = Exhibit.new({name: "IMAX",cost: 15})
-
+    @patron_1 = Patron.new("Bob", 20)
+    @patron_2 = Patron.new("Sally", 20)
   end
   describe "#initializze" do
     it 'exists and has attr' do
@@ -30,5 +31,19 @@ RSpec.describe Museum do
       expect(@dmns.exhibits).to eq([@gems_and_minerals, @dead_sea_scrolls, @imax])
 
     end
+  end
+
+  describe 'recommend_exhibits'do
+   it 'reccommend exhibits' do
+      @patron_1.add_interest("Dead Sea Scrolls")
+      @patron_1.add_interest("Gems and Minerals")
+      @patron_2.add_interest("IMAX")
+
+      @dmns.recommend_exhibits(@patron_1)
+      @dmns.recommend_exhibits(@patron_2)
+
+      expect(@dmns.recommend_exhibits(@patron_1)).to eq[@dead_sea_scrolls, @gems_and_minerals]
+      expect(@fmns.recommend_exhibits(patron_2)).to eq[@imax]
+   end
   end
 end
