fe9179f28ea13bbe6779aadfa24102b6c6430a81
Maia Bard <Maiakbard@gmail.com>
2025-02-25 09:57:39 -0700

    Add intilaize and attr_reader to museum class and fix describe museum block and change all @dmns to correct spelling

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -7,20 +7,23 @@ RSpec.configure do |config|
   config.formatter = :documentation
 end
 
-RSpec.describe Patron do
+RSpec.describe Museum do
   before(:each) do
     @dmns = Museum.new("Denver Museum of Nature and Science")
   end
 
   describe '#intialize' do
    it 'exsists' do
-      expect(@dms).to be_a(Museum)
+      expect(@dmns).to be_a(Museum)
     end
   end
 
   describe '#attributes' do
     it 'has a name and a cost' do
-      expect(@dms.name).to eq("Denver Museum of Nature and Science")
-      expect(@dms.exhibits).to eq([])
+      expect(@dmns.name).to eq("Denver Museum of Nature and Science")
+      expect(@dmns.exhibits).to eq([])
     end
   end
+end
+
+
