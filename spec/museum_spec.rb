853a0251860681f36f2ae130547809926653221e
Brady Blair <bblair321@yahoo.comm>
2025-02-25 09:22:34 -0800

    added a test for shoveling exhibits into the @exhibits array

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -5,7 +5,9 @@ require './lib/museum'
 RSpec.describe Museum do
   before(:each) do
     @dmns = Museum.new("Denver Museum of Nature and Science")
-
+    @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    @imax = Exhibit.new({name: "IMAX",cost: 15})
   end
 
   describe "#initialize" do
@@ -16,6 +18,13 @@ RSpec.describe Museum do
 
     it 'has an empty exhibits array' do
       expect(@dmns.exhibits).to eq([])
+    end
+  end  
+  describe "#add_exhibit" do
+    it 'adds an exhibit to the museum' do
+      @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+      @dmns.add_exhibit(@gems_and_minerals)
+      expect(@dmns.exhibits).to include(@gems_and_minerals)
     end
   end  
 end  
