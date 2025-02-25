83f1a029e611100f9f363862280f088f70a5ff1a
Maia Bard <Maiakbard@gmail.com>
2025-02-25 09:35:17 -0700

    Add interests method into attributes tests and create a add_interests test

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -20,11 +20,16 @@ RSpec.describe Patron do
     it 'has a name and a cost' do
       expect(@patron_1.name).to eq("Gems and Minerals")
       expect(@patron_1.spending_money).to eq(20)
+      expect(@patron_1.interests).to eq([])
     end
   end
 
-  describe '#spending_money' do
-    it ''
+  describe 'add_interests' do
+    it 'can add interests'
+      expect(@patron_1.interests).to eq([])
+      @patron_1.add_interest("Dead Sea Scrolls")
+      @patron_1.add_interest("Gems and Minerals")
+      expect(@patron_1.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
   end
 
 end
