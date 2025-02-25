af8fef135c7b203fc4c1572bc1d066bf1073987b
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-02-25 11:12:45 -0500

    Adds in Exhibit class test for iteration 1

---
+++ spec/exhibit_spec.rb
@@ -0,0 +1,20 @@
+require './lib/exhibit'
+require './lib/museum'
+require './lib/patron'
+
+RSpec.describe Exhibit do
+    describe '#initialize' do
+        it 'exists' do
+            exhibit = Exhibit.new9({name: "Gems and Minerals", cost: 0})
+
+            expect(exhibit).to be_a(Exhibit)
+        end
+
+        it 'assigns attributes correctly' do
+            exhibit = Exhibit.new9({name: "Gems and Minerals", cost: 0})
+
+            expect(exhibit.name).to eq("Gems and Minerals")
+            expect(exhibit.cost).to eq(0)
+        end
+    end
+end
---
+++ spec/patron_spec.rb
@@ -0,0 +1,7 @@
+require './lib/exhibit'
+require './lib/museum'
+require './lib/patron'
+
+RSpec.describe Patron do
+    
+end
