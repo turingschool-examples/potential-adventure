9486a731261d9a28f3740631886e436d1819bf56
Stephanie Guerrero <sguerrero0991@gmail.com>
2025-02-25 11:22:55 -0600

    Will add to the Museum class and adjust tests  in musuem_spec.rb file to compelte iteration 2

---
+++ lib/museum.rb
@@ -0,0 +1,15 @@
+class Museum 
+  attr_reader :name, :exhibits
+  def initialize(name)
+    @name = name
+    @exhibits = []
+  end
+
+  def add_exhibit(exhibit)
+    @exhibits << exhibit
+  end
+
+  def recommend_exhibits(patron)
+    @exhibits.select { |exhibit| patron.interests.include?(exhibit.name) }
+  end
+end
--- spec/musuem_spec.rb
+++ spec/musuem_spec.rb
@@ -3,9 +3,9 @@ require './lib/exhibit'
 require './lib/patron'
 require 'pry'
 
-RSpec.describe Musuem do 
+RSpec.describe Museum do 
   describe '#initialize' do
-    it 'is a Musuem' do
+    it 'is a Museum' do
       dmns = Museum.new("Denver Museum of Nature and Science")
       expect(dmns).to be_a(Museum)
     end
@@ -23,6 +23,8 @@ RSpec.describe Musuem do
 
   describe '#initialize' do
     it 'is an Exhibit' do
+      dmns = Museum.new("Denver Museum of Nature and Science")
+      expect(dmns).to be_a(Museum)
       gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
       dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
       imax = Exhibit.new({name: "IMAX",cost: 15})
@@ -32,10 +34,12 @@ RSpec.describe Musuem do
     end
 
     it 'adds exhibits' do
+      dmns = Museum.new("Denver Museum of Nature and Science")
+      expect(dmns).to be_a(Museum)
       gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
       dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
       imax = Exhibit.new({name: "IMAX",cost: 15})
-      xpect(gems_and_minerals).to be_a(Exhibit)
+      expect(gems_and_minerals).to be_a(Exhibit)
       expect(dead_sea_scrolls).to be_a(Exhibit)
       expect(imax).to be_a(Exhibit)
 
@@ -49,12 +53,15 @@ RSpec.describe Musuem do
 
   describe '#Patron' do
     it 'adds more patrons and interests' do
+      dmns = Museum.new("Denver Museum of Nature and Science")
+      expect(dmns).to be_a(Museum)
       gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
       dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
       imax = Exhibit.new({name: "IMAX",cost: 15})
       expect(gems_and_minerals).to be_a(Exhibit)
       expect(dead_sea_scrolls).to be_a(Exhibit)
       expect(imax).to be_a(Exhibit)
+     
 
       patron_1 = Patron.new("Bob", 20)
       expect(patron_1).to be_a(Patron)
@@ -67,7 +74,29 @@ RSpec.describe Musuem do
     end
 
     it 'recommends exhibits' do
-      expect(dmns.recommend_exhibits(patron_1)).to eq([dead_sea_scrolls, gems_and_minerals])
+      dmns = Museum.new("Denver Museum of Nature and Science")
+      expect(dmns).to be_a(Museum)
+      gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+      dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+      imax = Exhibit.new({name: "IMAX",cost: 15})
+      expect(gems_and_minerals).to be_a(Exhibit)
+      expect(dead_sea_scrolls).to be_a(Exhibit)
+      expect(imax).to be_a(Exhibit)
+
+      dmns.add_exhibit(gems_and_minerals)
+      dmns.add_exhibit(dead_sea_scrolls)
+      dmns.add_exhibit(imax)
+     
+      patron_1 = Patron.new("Bob", 20)
+      expect(patron_1).to be_a(Patron)
+      patron_1.add_interest("Dead Sea Scrolls")
+      patron_1.add_interest("Gems and Minerals")
+
+      patron_2 = Patron.new("Sally", 20)
+      expect(patron_2).to be_a(Patron)
+      patron_2.add_interest("IMAX")
+
+      expect(dmns.recommend_exhibits(patron_1)).to eq([gems_and_minerals, dead_sea_scrolls])
       expect(dmns.recommend_exhibits(patron_2)).to eq([imax])
     end
   end
