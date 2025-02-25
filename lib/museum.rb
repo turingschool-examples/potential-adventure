de553c760505e40fa78d8008db385953f4f2b885
Katya Weicht <katya.weicht@gmail.com>
2025-02-25 09:52:12 -0700

    Write tests and method for adding exhibit

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,6 +1,11 @@
 class Museum
-    attr_reader :name
+    attr_reader :name, :exhibits
     def initialize(name)
         @name = name
+        @exhibits = []
+    end
+
+    def add_exhibit(exhibit)
+        @exhibits << exhibit
     end
 end
--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -13,4 +13,24 @@ RSpec.describe Museum do
         it 'has a name' do
             expect(@dmns.name).to eq("Denver Museum of Nature and Science")
         end
+    describe "exhibits" do
+        it 'has exhibits' do
+            expect(@dmns.exhibits).to eq([])
+        end
+        it 'can add exhibits' do
+            gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+            dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+            imax = Exhibit.new({name: "IMAX",cost: 15})
+
+            expect(@dmns.exhibits).to eq([])
+
+            @dmns.add_exhibit(gems_and_minerals)
+            @dmns.add_exhibit(dead_sea_scrolls)
+            @dmns.add_exhibit(imax)
+
+            expect(@dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
+
+        end
+        
+    end
 end
