1a4a14f4a57c80dd515e6c794d0c6963081c62cf
Maia Bard <Maiakbard@gmail.com>
2025-02-25 09:51:51 -0700

    Add tests for intialize and attributes for museum spec

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -6,3 +6,21 @@ require './lib/exhibit'
 RSpec.configure do |config|
   config.formatter = :documentation
 end
+
+RSpec.describe Patron do
+  before(:each) do
+    @dmns = Museum.new("Denver Museum of Nature and Science")
+  end
+
+  describe '#intialize' do
+   it 'exsists' do
+      expect(@dms).to be_a(Museum)
+    end
+  end
+
+  describe '#attributes' do
+    it 'has a name and a cost' do
+      expect(@dms.name).to eq("Denver Museum of Nature and Science")
+      expect(@dms.exhibits).to eq([])
+    end
+  end
