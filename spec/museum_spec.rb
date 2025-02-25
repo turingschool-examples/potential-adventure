45525d74faa7ba8811bc65b9ab711bd4475dfff1
Ecaterina Profir <ecaterina.profir@gmail.com>
2025-02-25 10:58:31 -0600

    I created a test to check if exhibits cab be added to the class object through the add_exhibits methods. I passed in three examples of exhibits to check the behaviour of the method.

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -5,12 +5,30 @@ require './lib/exhibit'
 RSpec.describe Museum do
   before(:each)do
     @dmns = Museum.new("Denver Museum of Nature and Science")
+    @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    @imax = Exhibit.new({name: "IMAX",cost: 15})
+
   end
   describe "#initializze" do
     it 'exists and has attr' do
       expect(@dmns).to be_a(Museum)
       expect(@dmns.name).to eq("Denver Museum of Nature and Science")
       expect(@dmns.exhibits).to eq([])
+    end
+  end
+
+  describe '#add_exhibit' do
+    it 'adds exhibits' do
+      @dmns.add_exhibit(@gems_and_minerals)
+      expect(@dms.exhibits).to eq(@gems_and_minerals)
+
+      @dmns.add_exhibit(@dead_sea_scrolls)
+      expect(@dms.exhibits).to eq(@gems_and_minerals, @dead_sea_scrolls)
+
+      @dmns.add_exhibit(@imax)
+      expect(@dms.exhibits).to eq(@gems_and_minerals, @dead_sea_scrolls, @imax)
+
     end
   end
 end
