e26580d422383a5dcce06f3f2c19ad76b71df6bf
Calvin Suiter <cdsuiter@yahoo.com>
2025-02-25 13:57:23 -0500

    ic submission

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -10,6 +10,13 @@ end
 RSpec.describe Museum do 
     before(:each) do
         @dmns = Museum.new("Denver Museum of Nature and Science")
+        @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+        @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+        @imax = Exhibit.new({name: "IMAX",cost: 15})
+        @patron_1 = Patron.new("Bob", 20)
+        @patron_2 = Patron.new("Sally", 20)
+        @patron_3 = Patron.new("Johnny", 5)
+
     end
 
     it 'exist' do
@@ -25,11 +32,6 @@ RSpec.describe Museum do
     end
 
     it 'can add exhibits' do
-    
-        @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
-        @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
-        @imax = Exhibit.new({name: "IMAX",cost: 15})
-
         @dmns.add_exhibit(@gems_and_minerals)
         @dmns.add_exhibit(@dead_sea_scrolls)
         @dmns.add_exhibit(@imax)
@@ -38,16 +40,9 @@ RSpec.describe Museum do
     end
 
     it 'can recommend exhibits to patrons' do
-        @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
-        @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
-        @imax = Exhibit.new({name: "IMAX",cost: 15})
-    
         @dmns.add_exhibit(@gems_and_minerals)
         @dmns.add_exhibit(@dead_sea_scrolls)
         @dmns.add_exhibit(@imax)
-
-        @patron_1 = Patron.new("Bob", 20)
-        @patron_2 = Patron.new("Sally", 20)
 
         @patron_1.add_interest("Dead Sea Scrolls")
         @patron_1.add_interest("Gems and Minerals")
