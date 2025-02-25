a28ce132fbe991560b37bf9bd23f85b4d0d543cc
Brady Blair <bblair321@yahoo.comm>
2025-02-25 09:12:19 -0800

    Added an @exhibits instance variable. It initializes with an empty array. I then created a helper method to shovel exhbits into the @exhibits array

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,5 +1,11 @@
 class Museum
-  def initialize
-    
+  attr_reader :name, :exhibits
+  def initialize(name, exhibits = [])
+    @name = name
+    @exhibits = exhibits
+  end
+
+  def add_exhibits(exhibit)
+    @exhbibits << exhibit
   end
 end
