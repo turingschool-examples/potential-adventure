8455d59b29879ae8e1e622d3f4c241bbbaae59d6
Joe <53631725+JustJoeYo@users.noreply.github.com>
2025-02-25 08:23:22 -0800

    2/3 classes

---
+++ lib/patron.rb
@@ -0,0 +1,23 @@
+class Patron
+  def initialize(name, spending_money)
+    @name = name
+    @spending_money = spending_money
+    @interests = []
+  end
+
+  def name
+    @name
+  end
+
+  def spending_money
+    @spending_money
+  end
+
+  def interests
+    @interests
+  end
+
+  def add_interest(interest)
+    @interests << interest
+  end
+end
