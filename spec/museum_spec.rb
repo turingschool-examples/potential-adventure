f6394aca9c94c9ab7cae5bd4df979f21bc444ca7
Katya Weicht <katya.weicht@gmail.com>
2025-02-25 10:01:11 -0700

    Create tests for recommend exhibits method

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -31,6 +31,42 @@ RSpec.describe Museum do
             expect(@dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
 
         end
+        it 'can recommend exhibits' do
+            #add exhibits to the museum
+            gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+            dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+            imax = Exhibit.new({name: "IMAX",cost: 15})
+
+            @dmns.add_exhibit(gems_and_minerals)
+            @dmns.add_exhibit(dead_sea_scrolls)
+            @dmns.add_exhibit(imax)
+
+            expect(@dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
+
+            #create patron one and add interests
+
+            patron_1 = Patron.new("Bob", 20)
+
+            patron_1.add_interest("Dead Sea Scrolls")
+            patron_1.add_interest("Gems and Minerals")
+
+            #create patron two and add interests
+
+            patron_2 = Patron.new("Sally", 20)
+
+            patron_2.add_interest("IMAX")
+
+            #recommend exhibits to patron one
+
+            @dmns.recommend_exhibits(patron_1)
+            expect(@dmns.recommend_exhibits(patron_1)).to eq([dead_sea_scrolls, gems_and_minerals])
+
+            #recommend exhibits to patron two
+
+            @dmns.recommend_exhibits(patron_2)
+            expect(@dmns.recommend_exhibits(patron_2)).to eq([imax])
+
+        end
         
     end
 end
