874b61d1ed0a312a8b02424629f5218d451e653e
Brady Blair <bblair321@yahoo.comm>
2025-02-25 10:58:13 -0800

    Forgot to add my iteration stuff

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,8 +1,9 @@
 class Museum
-  attr_reader :name, :exhibits
+  attr_reader :name, :exhibits, :patrons
   def initialize(name, exhibits = [])
     @name = name
     @exhibits = exhibits
+    @patrons = []
   end
 
   def add_exhibit(exhibit)
@@ -14,4 +15,19 @@ class Museum
       patron.interests.include?(exhibit.name)
     end
   end
+
+  def admit(patron)
+    total_cost = 0
+    patron.interests.each do |interest|
+      exhibit = @exhibits.find { |exhibit| exhibit.name == interest }
+      total_cost += exhibit.cost if exhibit
+    end
+
+    if patron.spending_money >= total_cost
+       patron.spending_money -= total_cost
+      "Admitted to the museum, total cost: $#{total_cost}"
+    else
+      "Not enough money to admit to the museum"
+    end
+  end  
 end
