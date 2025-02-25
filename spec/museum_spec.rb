fc168e149b6a5810870381c85141925bcdfcd20d
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-02-25 11:40:52 -0500

    Adds in tests for iteration 2

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -32,4 +32,27 @@ RSpec.describe Museum do
             expect(dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
         end
     end
+
+    describe '#recommend_exhibits' do
+        it 'can reccomend exhibits based on patron interest' do
+            dmns = Museum.new("Denver Museum of Nature and Science")
+            gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+            dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+            imax = Exhibit.new({name: "IMAX",cost: 15})
+
+            dmns.add_exhibit(gems_and_minerals)
+            dmns.add_exhibit(dead_sea_scrolls)
+            dmns.add_exhibit(imax)
+
+            patron_1 = Patron.new("Bob", 20)
+            patron_2 = Patron.new("Sally", 20)
+
+            patron_1.add_interest("Dead Sea Scrolls")
+            patron_1.add_interest("Gems and Minerals")
+            patron_2.add_interest("IMAX")
+
+            expect(dmns.recommend_exhibits(patron_1)).to eq([gems_and_minerals, dead_sea_scrolls])
+            expect(dmns.recommend_exhibits(patron_2)).to eq([imax])
+        end
+    end
 end
