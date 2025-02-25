ab6f1e868e7e55ea19f74ed2a6214a7ef9e49a0d
Brady Blair <bblair321@yahoo.comm>
2025-02-25 08:25:01 -0800

    Added a test for the @name instance variable and @cost instance variable

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -2,13 +2,21 @@ require './lib/exhibit'
 
 RSpec.describe Exhibit do
   before(:each) do
-    @exhibit = Exhibit.new
+    @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
   end
 
   describe "#initialize" do
-      it 'Exhibit to be an instance of exhibit' do
+    it 'Exhibit to be an instance of exhibit' do
 
       expect(@exhibit).to be_an_instance_of(Exhibit)
+    end
+
+    it 'has a name' do
+      expect(@exhibit.name).to eq("Gems and Minerals")
+    end
+
+    it 'has a cost' do
+      expect(@exhibit.cost).to eq(0)
     end
   end
 end
